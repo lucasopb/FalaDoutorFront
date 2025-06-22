0925ebcce6be61d415f7ace5396366cffc311486
lucas oliveira <lucasoliveira.dpb@gmail.com>
2025-06-22 15:49:36 -0300

    config: setup deploy

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,7 +4,7 @@ import { Patient } from "@/types/patient";
 import { HealthInsurance } from "@/types/healthInsurance";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://faladoutorapi.onrender.com",
 });
 
 // ---------------- MÉDICOS ----------------
